9bbc99a7ded076f66ef05e88ef9fc49aff7ac78f
hilunaaa <ameliaaa.wl@gmail.com>
2025-06-16 08:58:49 +0700

    feat: design ui

--- GoFish/View/MenuView.swift
+++ GoFish/View/MenuView.swift
@@ -12,25 +12,22 @@ struct MenuView: View {
 
     var body: some View {
         VStack(spacing: 20) {
-            Text("Go Fish!")
-                .font(.system(size: 60, weight: .bold))
-                .foregroundColor(.blue)
+            Image("logo")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 300, height: 150)
+            .padding(.bottom, 20)
 
             Button {
                 matchManager.startMatchmaking()
             } label: {
-                Text("PLAY")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 80)
-                    .padding(.vertical, 15)
-                    .background(
-                        Capsule().fill(
-                            matchManager.authenticationState != .authenticated || matchManager.gameState != .menu
-                            ? Color.gray : Color.blue
-                        )
-                    )
-            }
+                Image("playbutton")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                        .opacity(matchManager.authenticationState != .authenticated || matchManager.gameState != .menu ? 0.5 : 1.0)
+                        .animation(.easeInOut, value: matchManager.authenticationState != .authenticated || matchManager.gameState != .menu)
+                }
             .disabled(matchManager.authenticationState != .authenticated || matchManager.gameState != .menu)
             
             if matchManager.authenticationState != .authenticated {
@@ -51,6 +48,13 @@ struct MenuView: View {
             }
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Image("menuviewbackground")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            )
     }
 }
 
